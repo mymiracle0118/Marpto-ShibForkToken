14dbbcadadcc3884967bd746a151ccfefd9bb9d7
mymircle0118 <noahsflood908@gmail.com>
2024-02-28 23:44:16 +0900

    buy fee test finish

--- src/ShibFork.sol
+++ src/ShibFork.sol
@@ -3,16 +3,111 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+// import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ShibFork is ERC20, ERC20Burnable, Ownable {
+import {console, console2} from "forge-std/Test.sol";
+
+contract ShibFork is ERC20, Ownable {
+
+    uint256 public feePercent;
+    address public feeReceiver;
+
+    uint256 public MAX_SUPPLY;
+    uint256 minted;
+
+    address alice = address(6);
+    address bob = address(7);
+
+    address routerAddress = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address wethAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address factoryAddress = address(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
+
+    address pool;
+
+    mapping(address => bool) whitelist;
+    mapping(address => bool) dex;
     constructor(address initialOwner)
-        ERC20("ShibFork", "SORK")
+        ERC20("ShibPork", "SORK")
         Ownable(initialOwner)
-    {}
+    {
+        feeReceiver = initialOwner;
+        feePercent = 50;
+        MAX_SUPPLY = 581_000_000_000_000_000000000000000000;
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
+        require(minted + amount <= MAX_SUPPLY, "Overflow total supply");
+        minted += amount;
         _mint(to, amount);
+    }
+
+    function setFeeReceiver(address to) public onlyOwner {
+        require(to != address(0x0), "Non zero address");
+        feeReceiver = to;
+    }
+
+    function setWhiteList(address to, bool _flag) public onlyOwner {
+        whitelist[to] = _flag;
+    }
+
+    function setDex(address to, bool _flag) public onlyOwner {
+        dex[to] = _flag;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
+        console.log("transferFrom function");
+        address spender = _msgSender();
+        _spendAllowance(from, spender, amount);
+
+        uint256 effectiveAmount = amount;
+        uint256 feeAmount;
+        if (feeAvailable(from, to)) {
+            feeAmount = calculateFeeAmount(amount, feePercent);
+            console.log("fee Amount");
+            console.log(feeAmount);
+            effectiveAmount = amount - feeAmount;
+            _transfer(from, feeReceiver, feeAmount);
+        }
+
+        _transfer(from, to, effectiveAmount);
+        return true;
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        console.log("transfer fucntion");
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance.");
+        uint256 effectiveAmount = amount;
+        uint256 feeAmount;
+        if (feeAvailable(msg.sender, to)) {
+            feeAmount = calculateFeeAmount(amount, feePercent);
+            console.log("fee Amount");
+            console.log(feeAmount);
+            effectiveAmount = amount - feeAmount;
+            _transfer(msg.sender, feeReceiver, feeAmount);
+        }
+        _transfer(msg.sender, to, effectiveAmount);
+        return true;
+    }
+
+    function feeAvailable(address from, address to) internal view returns (bool) {
+        console.log("feeAvailable function");
+        console.log("from address");
+        console.log(from);
+        console.log("to address");
+        console.log(to);
+        if(dex[from] == true || dex[to] == true) {
+            console.log("dex notification");
+            if(whitelist[from] == true || whitelist[to] == true) {
+                console.log("whitelist");
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    function calculateFeeAmount(uint256 amount, uint256 _feePercent) internal pure returns (uint256) {
+        return (amount * _feePercent) / 1000;
     }
 }
--- test/ShibFork.t.sol
+++ test/ShibFork.t.sol
@@ -2,19 +2,99 @@
 pragma solidity ^0.8.13;
 
 import {Test, console, console2} from "forge-std/Test.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import {ShibFork} from "src/ShibFork.sol";
+import "test/IWETH.sol";
 
 contract ShibForkTest is Test {
     ShibFork public shib;
-    address tokenOnwer = address(1);
 
+    address alice = address(1);
+    address bob = address(2);
+    address tokenOwner = address(3);
+
+    uint256 bscfork;
+    address routerAddress = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
+    address wbnbAddress = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+    address factoryAddress = 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
+    address poolAddress = 0xd0604cb5797Db2Eda79B82d13586F1eDd9de65a3;
+
+    address pool;
     function setUp() public {
-        shib = new ShibFork(tokenOnwer);
+        bscfork = vm.createFork(vm.envString("BSC_RPC_URL"));
+
+        vm.selectFork(bscfork);
+        assertEq(vm.activeFork(), bscfork);
+
+        vm.rollFork(36532683);
+
+        shib = new ShibFork(tokenOwner);
     }
 
-    function test_Increment() public {
+    function test_buy_shibFork() public {
+
+        test_createAndAddLiquidity();
+
+        IUniswapV2Router02 router = IUniswapV2Router02(routerAddress);
+
+        address[] memory path = new address[](2);
+        path[0] = wbnbAddress;
+        path[1] = address(shib);
+
+        vm.deal(alice, 5 ether);
+
+        vm.prank(tokenOwner);
+        shib.setDex(poolAddress, true);
+
+        vm.prank(alice);
+        // (bool success,) =
+        // Make the swap
+        router.swapExactETHForTokensSupportingFeeOnTransferTokens{ value: 1 ether }(
+            0, // Accept any amount of tokens
+            path,
+            address(alice), // Send the tokens to this contract
+            block.timestamp + 300 // Set a deadline for the swap
+        );
+
+        console.log("===========After buy=========");
+        console.log(shib.balanceOf(alice));
+        console.log(shib.balanceOf(shib.feeReceiver()));
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
+    function test_createAndAddLiquidity() public {
+        // Create the pair if it hasn't been initialized yet.
+        pool = IUniswapV2Factory(factoryAddress).createPair(address(shib), wbnbAddress);
+
+        console.log("pool address");
+        console.log(address(pool));
+
+        vm.deal(alice, 10 ether);
+
+        vm.prank(alice);
+        (bool success,) =
+            wbnbAddress.call{ value: 10 ether }(abi.encodeWithSignature("deposit(address,uint256)", alice, 10 ether));
+        if (!success) return;
+
+        console.log(IWETH(wbnbAddress).balanceOf(address(alice)));
+
+        vm.prank(tokenOwner);
+        shib.mint(address(alice), 10 ether);
+
+        vm.prank(alice);
+        shib.approve(address(routerAddress), 10 ether);
+
+        vm.startPrank(alice);
+        IWETH(wbnbAddress).approve(address(routerAddress), 10 ether);
+        console.log(IWETH(wbnbAddress).allowance(address(alice), address(routerAddress)));
+        shib.approve(address(routerAddress), 10 ether);
+        vm.stopPrank();
+
+        vm.prank(alice);
+        // Add liquidity to the Uniswap V2 pair
+        IUniswapV2Router02(routerAddress).addLiquidity(
+            address(shib), wbnbAddress, 10 ether, 10 ether, 9 ether, 9 ether, alice, block.timestamp
+        );
     }
 }
